83d677e225c3727716844e7bedb5d09c43d8b29d
gilifranco <gilifranco1@gmail.com>
2022-07-04 23:01:59 -0300

    desafio javascript

--- js/index.js
+++ js/index.js
@@ -52,16 +52,85 @@ while (opcion != 0) {
 const productos = [{
     id: 1,
     title: 'shampoode5litros',
+    valor: 2500,
+    imageURL: 'https://http2.mlstatic.com/D_NQ_NP_781356-MLA45262293126_032021-O.jpg',
     tags: ['shampoo', '5', 'bidon', 'litros'],
 }, {
     id: 2,
     title: 'shampoode1litro',
+    valor: 1200,
+    imageURL: 'http://d3ugyf2ht6aenh.cloudfront.net/stores/138/958/products/shampoo-color-master-editado1-cd3e407099adf75c4816179127298149-640-0.jpg',
     tags: ['shampoo', '1', 'litro', 'botella'],
 } ,{
     id: 3,
     title: 'shampooenpomo',
+    valor: 500,
+    imageURL: 'http://d3ugyf2ht6aenh.cloudfront.net/stores/138/958/products/shmapoo-cremoso-chiso-editado1-5345e8245489cd4b4a16179910799732-640-0.jpg',
     tags: ['shampoo','en', 'pomo'],
 
 }]
 
-console.log(productos.map(productos => productos.title));
+//console.log(productos.map(productos => productos.title));
+
+const carrito = []
+
+const contenedor = document.getElementById('contenedor');
+const contenedorCarrito = document.getElementById('carrito');
+
+const renderProducts = (products, target) => {
+    let acumulador = '';
+
+    products.map(product => {
+        acumulador += `
+        <div class="card m-2" style="width: 18rem;">
+            <img src="${product.imageURL}" class="card-img-top" alt="${product.title}">
+            <div class="card-body">
+                <h5 class="card-title">${product.title}</h5>
+                <p class="card-text">Precio: $${product.valor}</p>
+                <button ref=${product.id} class="btn btn-primary button">Comprar</button>
+            </div>
+        </div>
+        `
+    })
+
+    target.innerHTML = acumulador;
+
+    const buttons = document.querySelectorAll('.button');
+    buttons.forEach(button => button.addEventListener('click', handleClick));
+}
+
+const handleClick = (event) => {
+    const id = parseInt(event.target.getAttribute('ref'));
+    const product = productos.find(producto => producto.id === id);
+    alert('compraste ' + product.title + ' por $' + product.valor);
+    
+    if (carrito.some(el => el.id === product.id)) {
+        const posicion = carrito.findIndex(el => el.id === product.id)
+        carrito[posicion].cantidad = carrito[posicion].cantidad + 1;
+    } else {
+        carrito.push({
+            id: product.id,
+            title: product.title,
+            valor: product.valor,
+            cantidad: 1,
+            imageURL: product.imageURL
+        })
+    }
+    renderProducts(carrito, contenedorCarrito);
+}
+
+const buscador = (array, texto) => {
+    return array.filter(producto => producto.title.toLowerCase().includes(texto.toLowerCase()))
+}
+const form = document.getElementById('form');
+const input = document.getElementById('searchInput');
+
+const buscar = (e) => {
+    e.preventDefault();
+
+    renderProducts(buscador(productos, input.value), contenedor);
+}
+
+input.addEventListener('input', buscar);
+
+renderProducts(productos, contenedor);
