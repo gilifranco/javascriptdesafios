3e1eb36c6e4eabd076e888757b5665344928b85e
gilifranco <gilifranco1@gmail.com>
2022-07-07 00:19:59 -0300

    desafio javascript

--- js/index.js
+++ js/index.js
@@ -1,53 +1,3 @@
-//Pregunta: Con quien hablamos
-
-let bienvenida = parseInt ("Calculator 300")
-
-function saludoGenerico () {
-    let nombre = prompt("Tu nombre es:")
-    let apellido = prompt("Apellido:")
-    let edad = prompt ("Que edad tienes:")
-    const anio =2022
-    let anioNacimiento = anio - edad
-    alert (`Bienvenido a Calculator! ${nombre} ${apellido},tenes ${edad} y naciste en el año ${anioNacimiento}`)
-}
-
-saludoGenerico ()
-// Calculadora
-let opcion = parseInt(prompt('A calcular!, ingrese: \n 1- para sumar. \n 2- para restar. \n 3- para multiplicar. \n 0- para salir'));
-
-while ((opcion < 1 || opcion > 3) && opcion != 0 && opcion) {
-    alert('opcion no válida')
-    opcion = parseInt(prompt('por favor, ingrese: \n 1- para sumar. \n 2- para restar. \n 3- para multiplicar.'));
-};
-while (opcion != 0) {
-    let numeroA;
-    let numeroB;
-
-    switch (opcion) {
-        case 1: 
-            numeroA = parseInt(prompt('ingrese un numero'))
-            numeroB = parseInt(prompt('ingrese otro numero'))
-
-            alert ('el resultado es :' + (numeroA + numeroB))
-            break;
-        case 2: 
-            numeroA = parseInt(prompt('ingrese un numero'))
-            numeroB = parseInt(prompt('ingrese otro numero'))
-
-            alert ('el resultado es :' + (numeroA - numeroB))
-            break;
-        case 3: 
-            numeroA = parseInt(prompt('ingrese un numero'))
-            numeroB = parseInt(prompt('ingrese otro numero'))
-
-            alert ('el resultado es :' + numeroA * numeroB)
-            break;
-
-        default:
-            alert('opcion no valida')
-    }
-    opcion = parseInt(prompt('ingrese una opcion'))
-}
 
 const productos = [{
     id: 1,
@@ -74,7 +24,7 @@ const productos = [{
 
 const carrito = []
 
-const contenedor = document.getElementById('contenedor');
+const contenedorr = document.getElementById('contenedorr');
 const contenedorCarrito = document.getElementById('carrito');
 
 const renderProducts = (products, target) => {
@@ -128,9 +78,105 @@ const input = document.getElementById('searchInput');
 const buscar = (e) => {
     e.preventDefault();
 
-    renderProducts(buscador(productos, input.value), contenedor);
+    renderProducts(buscador(productos, input.value), contenedorr);
 }
 
 input.addEventListener('input', buscar);
 
-renderProducts(productos, contenedor);
+renderProducts(productos, contenedorr);
+
+
+// Creo La clase alumno
+class Alumno{
+
+  constructor(nombre,email,password){
+   this.nombre= nombre;
+   this.email= email;
+   this.password= password;
+ }
+}
+
+// Defino todas las variables globales que necesito para trabajar con el DOM
+let arrayAlumnos = [] ;
+let miFormulario = document.querySelector("#formulario");
+let inputNombre = document.querySelector("#iNombre");
+
+let nombreI = formulario.children[1].value;
+let emailI = formulario.children[3].value;
+let passwordI = formulario.children[5].value;
+let contenedor = document.querySelector("#alumnoIngresado")
+let displayTodos = document.querySelector("#displayTodos");
+let parrafos = display
+let bandera = false;
+// Defino Los Eventos de ambos botones
+miFormulario.addEventListener("submit",agregarAlumnos);
+btnMostrar.addEventListener('click',MostrarTodosAlumnos);
+                     Todos.getElementsByTagName("p");
+// Pongo en Focus el primer input
+inputNombre.focus();
+
+// FUNCIONES
+// FUNCION PARA COMPROBAR EL INGRESO DE DATOS EN INPUTS
+function validarForm() {
+nombreI=formulario.children[1].value;
+emailI=formulario.children[3].value;
+passwordI=formulario.children[5].value;
+console.log(nombreI);
+console.log(emailI);
+console.log(passwordI);
+
+if(nombreI == '' || emailI == '' || passwordI == '' ) { 
+alert('ERROR- Debe completar todos los campos para continuar');
+inputNombre.focus();
+bandera = false;
+} else {
+bandera=true;
+}
+}
+// FUNCION PARA AGREGAR ALUMNOS AL ARRAY DE ALUMNOS
+function agregarAlumnos(e) {
+    e.preventDefault();
+   validarForm()
+   // valido que esten ok Los datos del formulario
+    if(bandera == true) {
+     let opcion = confirm("Esta seguro de agregar el Alumno?");
+     if (opcion == true) {
+       let formulario = e.target
+       arrayAlumnos.push (new Alumno(nombreI,emailI,passwordI));
+                       }
+        else { alert ('No se agregara al usuario'); }
+    
+    formulario.children[1].value ='';
+    formulario.children[3].value = '';
+    formulario.children[5].value = '';
+
+    contenedor.innerHTML= '';
+    AgregarAlDom();
+    inputNombre.focus();
+} else { 
+    inputNombre.focus();
+}  
+}
+   
+// funcion para Mostrar en DOM el ultimo alumno ingresado
+function AgregarAlDom(){
+    contenedor.innerHTML= 
+    `<h3>Ultimo alumno Ingresado:</h3>
+      <p><strong>Nombre:</strong>${nombreI}</p>
+      <p><strong>Email:</strong>${emailI}</p>
+      <p><strong>Password:</strong>${passwordI}</p>
+      <hr>`;
+   }
+
+ // funcion para mostrar proceduralmente todos los alumnos en un div en DOM
+function MostrarTodosAlumnos(e) {
+    e.preventDefault();
+   leti=0;
+   displayTodos.innerHTML='<h3>Listado de todos los Alumnos:</h3>';
+   for(const alumno of arrayAlumnos){
+     displayTodos.innerHTML+= `
+     <p><strong>Nombre:</strong>${nombreI}</p>
+     <p><strong>Email:</strong>${emailI}</p>
+     <p><strong>Password:</strong>${passwordI}</p>
+     <hr>`;
+  } } 
